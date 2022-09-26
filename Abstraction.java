60e8730156af65418375f8538fe91d5d180e57ae
Abhishek singh <114073968+abhikabhishek1100@users.noreply.github.com>
2022-09-26 15:26:16 +0530

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+abstract class Abstraction {
+	//method of abstract class
+public void display() {
+	System.out.println("this is java programing");
+}
+}
+
+
---
+++ Testabstract.java
@@ -0,0 +1,12 @@
+package CoreJava;
+
+public class Testabstract extends Abstraction {
+ 
+	public static void main(String[] args) {
+		// creating an object of sub class
+		Testabstract obj = new Testabstract();
+		// access method of abstract class using object of sub class
+	 obj.display();	
+	}
+
+}
