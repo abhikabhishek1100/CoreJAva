5920b6c6d3c2d43ebdf481d9a673826f19639ddd
Abhishek singh <114073968+abhikabhishek1100@users.noreply.github.com>
2022-09-29 17:17:18 +0530

    Add files via upload

---
+++ LabSession/29-09-2022/FindingVersion.java
@@ -0,0 +1,13 @@
+/*Program to finding the current version of java
+  Author :- Abhishek Singh
+  Date :- 29/09/2022
+ */
+package CoreJava;
+// Creating class 
+class FindingVersion
+{
+public static void main(String[] args) 
+{
+System.out.println("The Current Version of Java is :" + System.getProperty("java.version"));
+}
+}
---
+++ LabSession/29-09-2022/Inheritance.java
@@ -0,0 +1,18 @@
+/*
+ Inherits the property of Child and Parent
+ */
+
+package CoreJava;
+
+public class Inheritance extends Child
+{
+public static void main(String[] args)
+{
+//Creating objects 
+Inheritance I = new Inheritance();
+//Calling the methods
+I.sing();
+I.speak();
+I.dance();
+}
+}
