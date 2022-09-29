43f6af3a5979eae4bf1d253cf990b5c246398740
Abhishek singh <114073968+abhikabhishek1100@users.noreply.github.com>
2022-09-29 17:18:56 +0530

    Add files via upload

---
+++ LabSession/29-09-2022/Child.java
@@ -0,0 +1,18 @@
+/*
+ Inherits the property of parents
+ Author :- Abhishek Singh
+ Date :- 29/09/2022
+ */
+package CoreJava;
+// Creating child class which inherits the property of parent class
+ class Child extends Parent {
+//Creating methods of child class
+void speak()
+{
+System.out.println("I can speak very well french language");
+}
+void dance() 
+{
+System.out.println("I can dance very well on english songs");
+}
+}
---
+++ LabSession/29-09-2022/Parent.java
@@ -0,0 +1,20 @@
+/*
+ Ques1:create a class parent with default constructor and void sing method.print something inside the constructor and method
+Create child class which will inherit the property of parent and has methods void speak and void dance.print something inside the two methods.
+Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program
+ 
+ Author :- Abhishek Singh
+ Date :- 29/09/2022
+
+ */
+// Multilevel Inheritance
+package CoreJava;
+// Creating parent class
+class Parent 
+{
+// Creating constructor 	
+void sing() 
+{
+System.out.println("--I can sing vry well--");
+}
+}
