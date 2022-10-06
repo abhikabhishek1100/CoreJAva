250127106628824ef53b36482e1cf9c53472730d
Abhishek singh <114073968+abhikabhishek1100@users.noreply.github.com>
2022-10-06 15:06:18 +0530

    Add files via upload

---
+++ LabSession/06-10-2022/LeapYear.java
@@ -0,0 +1,28 @@
+/*
+ Program to find that the given year is a Leap year or not
+ Author :- Abhishek_Singh
+ Date:- 06/10/2022
+ */
+
+package CoreJava;
+import java.util.Scanner;
+public class LeapYear 
+{
+public static void main(String[] args) 
+{
+// Creating Scanner object to take the values from the user 
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the year to check that it is leap year or not :");
+int year = sc.nextInt();
+
+// Applying condition to check that the entered year is leap year or not 
+if(year%4==0)
+{
+System.out.println("This is the leap year : " + year);	
+}
+else
+{
+System.out.println("This is not the leap year : " + year);
+}
+}
+}
